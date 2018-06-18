6b4f755d5be0bbd0082304696eaaf0e8e397d3b4
Roman Coedo <romancoedo@gmail.com>
2018-06-18 14:01:50 +0200

    Only use in interactive mode

--- functions/__auto_ls.fish
+++ functions/__auto_ls.fish
@@ -7,13 +7,11 @@ function __auto_ls_handle_dir --on-variable auto_ls_dir
 end
 
 function __auto_ls_handle_dirprev --on-variable dirprev
-    if status --is-command-substitution
-        return $status
-    end
-
-    set --local previous_dir $dirprev[(count $dirprev)]
-    if test ! "$auto_ls_dir" = "$previous_dir"
-        set -g auto_ls_dir $previous_dir
+    if status --is-interactive
+        set --local previous_dir $dirprev[(count $dirprev)]
+        if test ! "$auto_ls_dir" = "$previous_dir"
+            set -g auto_ls_dir $previous_dir
+        end
     end
 end
 
