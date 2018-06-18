fbf5f6de6c2ab373ea5a163d03bba15c7d8ce4c1
Roman Coedo <romancoedo@gmail.com>
2018-06-18 13:57:57 +0200

    Improve auto ls

--- functions/__auto_ls.fish
+++ functions/__auto_ls.fish
@@ -1,4 +1,19 @@
-function __auto_ls --on-variable PWD
-    ls
+function __auto_ls_handle_dir --on-variable auto_ls_dir
+    if test ! -z "$auto_ls_dir"
+        echo \n"--------------------------------------"
+        ls
+        echo "--------------------------------------"\n
+    end
 end
 
+function __auto_ls_handle_dirprev --on-variable dirprev
+    set --local previous_dir $dirprev[(count $dirprev)]
+    if test ! "$auto_ls_dir" = "$previous_dir"
+        set -g auto_ls_dir $previous_dir
+    end
+end
+
+function __auto_ls
+    __auto_ls_handle_dir
+    __auto_ls_handle_dirprev
+end
